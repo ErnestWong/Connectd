cd113a3e242553a7094e79569d912238abfb24ab
Ernest Wong <ewong2222@hotmail.com>
2015-03-01 14:47:09 -0500

    remove friendships table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228173439) do
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20150301194542) do
 
   create_table "invitations", force: :cascade do |t|
     t.integer  "user_id"
