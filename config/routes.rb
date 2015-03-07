fdb6d68dd6637ee5c49fbe67e210bc01855ee34a
Peter Chu <cgpchu@uwaterloo.ca>
2015-03-07 10:44:20 -0500

    add debug gems and create invite form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show]
-  resources :invitations, only: [:create, :new]
+  resources :invitations, :controller => 'invitations', only: [:new, :create, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
