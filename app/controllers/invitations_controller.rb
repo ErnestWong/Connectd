d8a51008fbb07bcffd4fc5d11141b3b733ac6802
Peter Chu <cgpchu@uwaterloo.ca>
2015-03-22 01:34:57 -0400

    Finish feature - allow users to search for other users, and to send invitations by clicking on a button on a user's profile page

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,10 +2,11 @@ class InvitationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @invitation = Invitation.new
     @invitation = current_user.invitations.build(invitation_params)
     if @invitation.save
       flash[:notice] = "invitation sent"
-      @user = User.find_by_username!('peterchu')
+      @user = User.find_by_id!(params[:friend_id])
       render 'users/show'
     else
       flash[:notice] = "invitation failed"
@@ -23,6 +24,6 @@ class InvitationsController < ApplicationController
 
 protected
   def invitation_params
-    params.require(:invitation).permit(:friend_id)
+    params.permit(:friend_id)
   end
 end
