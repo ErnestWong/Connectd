cc356328244ba992214fb612cad81c52c7eea0ba
Ernest Wong <ewong2222@hotmail.com>
2015-04-26 11:37:38 -0400

    user model tests

--- spec/models/social_profile_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SocialProfile, type: :model do
-  subject { build_stubbed :social_profile }
-
-  it 'should have a valid factory' do
-    expect(subject).to be_valid
-  end
-end
