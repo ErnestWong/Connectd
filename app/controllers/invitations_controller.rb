fa93e62dae2ae89b7243d8ec991bf7bbf30e70cc
Peter Chu <cgpchu@uwaterloo.ca>
2015-04-26 22:00:48 -0400

    ensure invitations are not sent twice

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,7 +4,12 @@ class InvitationsController < ApplicationController
   def create
     @invitation = Invitation.new
     @invitation = current_user.invitations.build(invitation_params)
-    if @invitation.save
+    existingInvitations = Invitation.where(user_id: current_user.id, friend_id: invitation_params[:friend_id])
+    if existingInvitations.length >= 1
+      flash[:notice] = "invitation already sent"
+      @user = User.find_by_id!(invitation_params[:friend_id])
+      render "users/show"
+    elsif @invitation.save
       flash[:notice] = "invitation sent"
       @user = User.find_by_id!(invitation_params[:friend_id])
       render 'users/show'
