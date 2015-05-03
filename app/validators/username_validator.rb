7152ec9510de4cdcc5b1a01a3049c24586eb1a58
Ernest Wong <ewong2222@hotmail.com>
2015-05-02 23:21:53 -0400

    styling errors

--- app/validators/username_validator.rb
+++ app/validators/username_validator.rb
@@ -2,7 +2,7 @@ class UsernameValidator < ActiveModel::Validator
   def validate(record)
     return if record.username.nil?
     unless record.username =~ /\A[a-zA-Z0-9_]+\Z/
-      record.errors[:username] << "must contain only alphanumeric or the characters: '_'"
+      record.errors[:username] << "must contain only alphanumeric characters or the characters: _"
     end
   end
 end
