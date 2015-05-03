7efdb4b4bc93d9b5b6e080b9bb5f9f20296e3fbf
Ernest Wong <ewong2222@hotmail.com>
2015-05-02 23:15:01 -0400

    form validator checkmark and error messages

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,6 +3,12 @@ namespace("C");
 C["users_show"] = 
   function() {
     var username_input = $("#username_input");
+    var valid_icon = $("#valid_icon");
+    var invalid_icon = $("#error_icon");
+    var valid = false;
+    valid_icon.hide();
+    invalid_icon.hide();
+
     var userid = window.location.pathname.match(/(\d+)\/*$/)[1];
 
     username_input.bind("change paste keyup", function() {
@@ -18,12 +24,21 @@ C["users_show"] =
         })
       ).then(
         function(data) {
-          updateStatus(data);
+          updateStatus(data.user_errors);
         }
       );
     }
 
-    function updateStatus() {
-      
+    function updateStatus(errors) {
+      valid = (errors.length == 0);
+
+      valid ? invalid_icon.hide() : valid_icon.hide();
+      valid ? valid_icon.show() : invalid_icon.show();
+
+      $(".user-profile-form-errors").empty();
+
+      _.each(errors, function(el, index, list) {
+        $(".user-profile-form-errors").append("<p>" + el.error + "</p>");
+      });
     }
   };
